73623be1e9548c51af7554d7df442bbc58f8854d
Parth Parmar <parthparmar4105@gmail.com>
2026-03-04 12:56:57 +0530

    Fix python script

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-```python
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Name: Parth</h1><h2>AppID: 2410244</h2><h3>Hobbies: Coding, Reading, Gaming</h3>'
+    return '<h1>Name: Parth</h1><h2>AppID: 101</h2><h3>Hobbies: Coding, Reading, Gaming</h3>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-```
+    app.run(host='0.0.0.0', port=5000)
