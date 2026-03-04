8d5623ca781ff4ae72c5073d0a5f2a4cfe933156
Parth Parmar <parthparmar4105@gmail.com>
2026-03-04 12:56:20 +0530

    Testing pipeline with hobbies

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-...
+```python
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Name: Parth</h1><h2>AppID: 2410244</h2><h3>Hobbies: Coding, Reading, Gaming</h3>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+```
